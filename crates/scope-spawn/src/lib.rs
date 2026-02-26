4173e73d646b2e376889f08c4811d461d433b8bd
Gary Pennington <garypen@gmail.com>
2026-02-26 15:02:16 +0000

    More docs and include the examples as doc comments

--- crates/scope-spawn/src/lib.rs
+++ crates/scope-spawn/src/lib.rs
@@ -2,6 +2,20 @@
 //! A Small utility library which aims to make structured concurrency a
 //! bit easier when used with tokio or tower.
 //!
+//! A Simple Example
+//!
+//! ```rust
+//! use scope_spawn::scope::Scope;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let scope = Scope::new();
+//!     scope.spawn(async {
+//!     println!("Hello from a spawned task!");
+//! });
+//! // scope is dropped here, and spawned tasks are cancelled.
+//! }
+//! ```
 #![warn(
     missing_docs,
     missing_debug_implementations,
